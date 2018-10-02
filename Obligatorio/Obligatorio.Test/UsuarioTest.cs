660e850a37ab0d1bb8e15ee17453de3070679d96
LAPTOP-UAT3JA23\Facundo <frevello16@hotmail.com>
2018-10-02 19:37:15 -0300

    Se crea el package test y la prueba unitaria UsuarioTest vacia

---
+++ Obligatorio/Obligatorio.Test/UsuarioTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Obligatorio.Test
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
