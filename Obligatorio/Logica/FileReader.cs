7db977d8e227370a95d1c442e65bafa48d59cdcb
LAPTOP-UAT3JA23\Facundo <frevello16@hotmail.com>
2018-10-04 20:36:47 -0300

    Refactoreo clase FileReader

--- Obligatorio/Logica/FileReader.cs
+++ Obligatorio/Logica/FileReader.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Excepciones;
 
 namespace Logica
@@ -11,11 +9,11 @@ namespace Logica
     public class FileReader
     {
         private StreamReader reader;
-
-        private Dictionary<String, int> variables;
+        private List<string> variables = new List<string>();
+        private int nroLinea = 1;
 
         private const String TIME = "TIME";
-        private const String VARDEF = "VARDEF=";
+        private const String VARDEF = "VARDEF";
         private const String FIN_REGISTRO = "#";
 
         public void OpenFile(string path)
@@ -35,29 +33,33 @@ namespace Logica
              reader = new StreamReader(path);
         }
 
-        public Boolean ValidarFormatoArchivo()
+        public void ValidarFormatoArchivo()
         {
-            LeerPrimeraLinea();
-            String linea = reader.ReadLine();
-            int nroLinea = 0;
-            while (linea != null )
-            {
-
-                
-            }
-
-            return false;
+            ValidarPrimeraLinea();
+            ValidarRegistros();
         }
 
-        private void LeerPrimeraLinea()
+        private void ValidarPrimeraLinea()
         {
             String linea = reader.ReadLine();
-            ValidarLineaContieneVARDEF(linea, 1);
-            ValidarLineaContieneTIME(linea, 1);
+            ValidarLineaContieneVARDEF(linea);
+            ValidarLineaContieneTIME(linea);
             CargarVariablesPrimeraLinea(linea);       
         }
 
-        private void ValidarLineaContieneVARDEF(String line, int nroLinea)
+        private void ValidarRegistros()
+        {
+            String linea = reader.ReadLine();
+            nroLinea++;
+            while (linea != null)
+            {
+                ValidarRegistro(linea);
+                linea = reader.ReadLine();
+                nroLinea++;
+            }
+        }
+
+        private void ValidarLineaContieneVARDEF(String line)
         {
             if (!line.StartsWith(VARDEF))
             {
@@ -65,7 +67,7 @@ namespace Logica
             }
         }
 
-        private void ValidarLineaContieneTIME(String line, int nroLinea)
+        private void ValidarLineaContieneTIME(String line)
         {
             if (!line.Contains(TIME))
             {
@@ -78,13 +80,13 @@ namespace Logica
             String[] vars = ObtenerVariablesPrimeraLinea(linea);
             for (int i = 0; i < vars.Length; i++)
             {
-                variables.Add(vars[i], 0);
+                variables.Add(vars[i]);
             }
         }
 
         private String[] ObtenerVariablesPrimeraLinea(String linea)
         {
-            EliminarDeLineaVARDEF(linea);
+            linea = EliminarDeLineaVARDEF(linea);
             return linea.Split(',');
         }
 
@@ -94,33 +96,78 @@ namespace Logica
             return lineaSplitIgual[1];
         }
 
-        private void LeerRegistro()
+        private void ValidarRegistro(String linea)
         {
-            String linea = reader.ReadLine();
-            int nroLinea = 1;
-            while (linea != null)
+            
+            List<string> variablesRegistro = new List<string>();
+            while (linea != null && !linea.Equals(FIN_REGISTRO))
             {
-                ValidarLineaContieneIgual(linea, nroLinea);
-                String[] var = SepararVariableDeDato(linea, nroLinea);
+                ValidarLineaContieneIgual(linea);
+                String[] vars = SepararVariableDeDato(linea);
+                String var = vars[0];
+                String dato = vars[1];
+                ValidarVariableExistaVARDEF(var);
+                ValidarVariableNoExistaEnRegistro(var, variablesRegistro);
+                ValidarFormatoDato(dato);
+                /////// AGREGAR EN DATA SETTT /////
+
+                variablesRegistro.Add(var);
+                linea = reader.ReadLine();
+                nroLinea++;
                 
+            }
+
+            if(linea == null)
+            {
+                throw new FileReaderException("ERROR: en linea " + nroLinea + " falta fin del registro");
+            }
+
+        }
+
+        private void ValidarVariableExistaVARDEF(String variable)
+        {
+            if (!variables.Contains(variable))
+            {
+                throw new FileReaderException("ERROR: en linea " + nroLinea + " variable no definida");
             }
         }
 
-        private void ValidarVariableExista()
+        private void ValidarVariableNoExistaEnRegistro(String variable, List<string> variablesRegistro)
         {
-            // validar que exista variable en variables
+            if (variablesRegistro.Contains(variable))
+            {
+                throw new FileReaderException("ERROR: en linea " + nroLinea + " variable repetida en registro");
+            }
+        }
+        private void ValidarFormatoDato(string dato)
+        {
+            try
+            {
+                float fltDato = float.Parse(dato);
+            }
+            catch (Exception e)
+            {
+                throw new FileReaderException("ERROR: en la linea " + nroLinea + " formato del dato incorrecto");
+            }
+        }
+        private void ValidarVariablesDeRegistroExisteTIME(List<string> variablesRegistro)
+        {
+            if (!variablesRegistro.Contains(TIME))
+            {
+                throw new FileReaderException("ERROR: en linea " + nroLinea + " variable TIME no definida");
+            }
         }
 
-        private void ValidarLineaContieneIgual(String linea, int nroLinea)
+        private void ValidarLineaContieneIgual(String linea)
         {
             if (!linea.Contains('='))
             {
-                throw new FileReaderException("ERROR: en linea " + nroLinea);
+                throw new FileReaderException("ERROR: en linea " + nroLinea + " falta simbolo =");
             }
         }
 
        
-        private String[] SepararVariableDeDato(String linea, int nroLinea)
+        private String[] SepararVariableDeDato(String linea)
         {
             String[] datos = linea.Split('=');
             if (datos.Length != 2)
--- Obligatorio/Obligatorio.Test/FileReaderTest.cs
+++ Obligatorio/Obligatorio.Test/FileReaderTest.cs
@@ -11,7 +11,7 @@ namespace Obligatorio.Test
     [TestClass]
     public class FileReaderTest
     {
-
+         
         [TestMethod]
         public void FileOpenTests()
         {
@@ -26,5 +26,78 @@ namespace Obligatorio.Test
             FileReader reader = new FileReader();
             reader.OpenFile(@"c:/file2.txt");
         }
+
+        [TestMethod]
+        public void ValidarFormatoArchivoTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"c:/file.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoSinVARDEFTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileSinVARDEF.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoSinTIMEenVARDEFTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileSinTIMEenVARDEF.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoSinIgualTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileSinIgual.txt");
+            reader.ValidarFormatoArchivo();
+        }
+        
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoNoExisteVarEnVARDEFTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileVarNoExisteEnVARDEF.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoVarNoRepetidaTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileVarRepetida.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoFormatoDatoIncorrectoTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileDatoIncorrecto.txt");
+            reader.ValidarFormatoArchivo();
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(FileReaderException))]
+        public void ValidarFormatoArchivoFormatoNoTerminaRegistroTest()
+        {
+            FileReader reader = new FileReader();
+            reader.OpenFile(@"C:\Users\Facundo\Desktop\Files\fileNoTerminaRegistro.txt");
+            reader.ValidarFormatoArchivo();
+        }
+
     }
 }
