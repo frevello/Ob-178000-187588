997900e05db095967994e0ae1133790f94a2f681
LAPTOP-UAT3JA23\Facundo <frevello16@hotmail.com>
2018-10-04 17:29:27 -0300

    Correción merge

---
+++ Obligatorio/Excepciones/UsuarioException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class UsuarioException: Exception
+    {
+        private const string MESSAGE = "ERROR AL CREAR USUARIO";
+
+        public UsuarioException() : base(MESSAGE)
+        {
+        }
+
+        public UsuarioException(String mensaje) : base(mensaje)
+        {
+        }
+
+        public UsuarioException(Exception innerException) : base(MESSAGE, innerException)
+        {
+        }
+
+    }
+}
