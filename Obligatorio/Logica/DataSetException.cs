ab9718a6547515d32cdadec58ca067fa2ee52d58
Equipo\Sofia <s.garciadst@gmail.com>
2018-10-14 18:36:49 -0300

    Se escibieron los Test para DataSetService.

---
+++ Obligatorio/Logica/DataSetException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class DataSetException: Exception
+    {
+        private const string MESSAGE = "ERROR DATASET";
+        public DataSetException() : base(MESSAGE)
+        {
+        }
+
+        public DataSetException(String mensaje) : base(mensaje)
+        {
+        }
+
+        public DataSetException(Exception innerException) : base(MESSAGE, innerException)
+        {
+        }
+    }
+}
---
+++ Obligatorio/Obligatorio.Test/DataSetServiceTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dominio;
+using System.Collections.Generic;
+
+namespace Obligatorio.Test
+{
+    [TestClass]
+    public class DataSetServiceTest
+
+    {
+        [TestMethod]
+        public void CargarDataSetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public IEnumerable<VariablesDataSet> GetRegistrosTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public IEnumerable<VariablesDataSet> GetRegistrosNoExisteDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public VariablesDataSet GetRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public VariablesDataSet GetRegistroNoExisteRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public String GetNombreDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public String GetNombreDataSetNoExisteDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public String GetRegistrosDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public String GetRegistrosDataSetNoExisteDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public IEnumerable<String> GetNombreRegistrosDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public IEnumerable<String> GetNombresRegistrosDataSetNoExisteDataSetTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public float GetPromedioRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public float GetPromedioRegistroNoExiteRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public float GetMinimoRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public float GetMinimoRegistroNoExiteRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public float GetMaximoRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public float GetMaximoRegistroNoExisteRegistroTest(DataSet dataSet, String nombreRegistro)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public float GetCantidadRegistrosTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataSetException))]
+        public float GetCantidadRegistrosNoExisteTest(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
